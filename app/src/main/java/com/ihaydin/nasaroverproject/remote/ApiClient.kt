7d4c91390bb6269f83726a1bb9a24f710cd93556
ihaydinn <ihaydin7@gmail.com>
2021-06-26 15:48:49 +0300

    methods added

--- app/src/main/java/com/ihaydin/nasaroverproject/remote/ApiClient.kt
+++ app/src/main/java/com/ihaydin/nasaroverproject/remote/ApiClient.kt
@@ -1,6 +1,8 @@
 package com.ihaydin.nasaroverproject.remote
 
 import com.ihaydin.nasaroverproject.Constant
+import com.ihaydin.nasaroverproject.entity.DataResponse
+import io.reactivex.Single
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
@@ -16,6 +18,13 @@ class ApiClient {
         .build()
         .create(ApiService::class.java)
 
+    fun getPhotosSelectedCamera(roverName: String, page: Int, camera: String): Single<DataResponse> {
+        return api.getPhotosSelectedCamera(roverName, page, camera)
+    }
+
+    fun getAllCameraPhotos(roverName: String, page: Int): Single<DataResponse> {
+        return api.getAllCameraPhotos(roverName, page)
+    }
 
     private fun getOkHttpClient(): OkHttpClient {
         val client = OkHttpClient.Builder()
