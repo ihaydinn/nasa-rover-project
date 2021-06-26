e0e1eaa4b8ceb49ef07b406528a5f74aea183972
ihaydinn <ihaydin7@gmail.com>
2021-06-26 15:03:06 +0300

    api client added

---
+++ app/src/main/java/com/ihaydin/nasaroverproject/remote/ApiClient.kt
@@ -0,0 +1,25 @@
+package com.ihaydin.nasaroverproject.remote
+
+import com.ihaydin.nasaroverproject.Constant
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+class ApiClient {
+
+    private val api = Retrofit.Builder()
+        .baseUrl(Constant.BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+        .client(getOkHttpClient())
+        .build()
+        .create(ApiService::class.java)
+
+
+    private fun getOkHttpClient(): OkHttpClient {
+        val client = OkHttpClient.Builder()
+        client.addInterceptor(RequestInterceptor())
+        return client.build()
+    }
+}
