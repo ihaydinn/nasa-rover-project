2df7a9600e9176bb3a6d06b5a75ea44f485db6c4
ihaydinn <ihaydin7@gmail.com>
2021-06-26 15:43:27 +0300

    get request added

--- app/src/main/java/com/ihaydin/nasaroverproject/Constant.kt
+++ app/src/main/java/com/ihaydin/nasaroverproject/Constant.kt
@@ -3,6 +3,6 @@ package com.ihaydin.nasaroverproject
 object Constant{
 
     val API_KEY = "hWb405BgzWzgVnPOQcCLHMQqbUSaq6o6CiOmMq1Y"
-    val BASE_URL = "https://api.nasa.gov/mars-photos/api/v1/rovers/"
+    val BASE_URL = "https://api.nasa.gov/mars-photos/api/v1/"
 
 }
--- app/src/main/java/com/ihaydin/nasaroverproject/remote/ApiService.kt
+++ app/src/main/java/com/ihaydin/nasaroverproject/remote/ApiService.kt
@@ -1,8 +1,24 @@
 package com.ihaydin.nasaroverproject.remote
 
+import com.ihaydin.nasaroverproject.entity.DataResponse
+import io.reactivex.Single
 import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface ApiService {
 
+    @GET("rovers/{roverName}/photos?sol=1000")
+    fun getPhotosSelectedCamera(
+        @Path("roverName") roverName: String,
+        @Query("page") page: Int,
+        @Query("camera") camera: String
+    ): Single<DataResponse>
+
+    @GET("rovers/{roverName}/photos?sol=1000")
+    fun getAllCameraPhotos(
+        @Path("roverName") roverName : String,
+        @Query("page") page : Int
+    ): Single<DataResponse>
 
 }
