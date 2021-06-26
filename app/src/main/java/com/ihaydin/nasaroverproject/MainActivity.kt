c18cef60909ed20005b0a2c3fd0e3f7ea1680805
ihaydinn <ihaydin7@gmail.com>
2021-06-27 00:51:05 +0300

    base activity added and implemented databinding

--- app/src/main/java/com/ihaydin/nasaroverproject/MainActivity.kt
+++ app/src/main/java/com/ihaydin/nasaroverproject/MainActivity.kt
@@ -1,29 +1,19 @@
 package com.ihaydin.nasaroverproject
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.ihaydin.nasaroverproject.base.BaseActivity
+import com.ihaydin.nasaroverproject.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding, MainActivityViewModel>() {
 
-    private lateinit var viewModel : MainActivityViewModel
+    override fun getLayoutRes(): Int = R.layout.activity_main
+
+    override fun getViewModel(): Class<MainActivityViewModel> = MainActivityViewModel::class.java
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        viewModel = ViewModelProviders.of(this).get(MainActivityViewModel::class.java)
-        viewModel.getPhotosSelectedCamera("curiosity", 1, "MAST")
-
-        viewModel.mPhotosSelectedCamera.observe(this, Observer {
-            it.let {
-            }
-        })
-
-        viewModel.mError.observe(this, Observer {
-            it.let {
-            }
-        })
     }
 }
---
+++ app/src/main/java/com/ihaydin/nasaroverproject/base/BaseActivity.kt
@@ -0,0 +1,24 @@
+package com.ihaydin.nasaroverproject.base
+
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProviders
+
+abstract class BaseActivity<DB: ViewDataBinding, VM: ViewModel>: AppCompatActivity() {
+    lateinit var dataBinding: DB
+    lateinit var viewModel: VM
+
+    @LayoutRes
+    abstract fun getLayoutRes(): Int
+    abstract fun getViewModel(): Class<VM>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(getViewModel())
+        dataBinding = DataBindingUtil.setContentView(this, getLayoutRes())
+    }
+}
